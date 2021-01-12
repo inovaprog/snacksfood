89a399bf408d2a207bb9e267547d78525827a1f8
Wandinho Oliveira <inovaprog@gmail.com>
2021-01-12 01:37:23 -0300

    APi

---
+++ pages/api/add_cliente/[...cliente].js
@@ -0,0 +1,35 @@
+import { connectToDatabase } from "../../../util/mongodb";
+
+export default async (req, res) => {
+  const {
+    query: { cliente },
+  } = req
+
+  const { db } = await connectToDatabase();
+
+  var nomeUser = cliente[0]
+  var telefoneUser = cliente[1]
+  
+
+  const user = await db
+    .collection("clientes")
+    .find({ telefone: cliente[1] })
+    .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
+
+  if (user.length == 0) {
+    await db.collection("clientes").insertOne(
+      {
+        nome: nomeUser,
+        telefone: telefoneUser,
+        qdtPontos: 0
+      })
+
+
+    res.send(200);
+  } else {
+    res.send(300);
+  }
+
+};
---
+++ pages/api/resgatar_cliente/[...parametros].js
@@ -0,0 +1,34 @@
+import { connectToDatabase } from "../../../util/mongodb";
+
+export default async (req, res) => {
+  const {
+    query: { parametros },
+  } = req
+
+  const { db } = await connectToDatabase();
+
+  var telefone = parametros[0]
+  var qtdPontos = parseInt(parametros[1])
+  
+
+  const user = await db
+    .collection("clientes")
+    .find({ telefone: telefone })
+    .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
+
+  if (user.length == 0) {
+
+    res.send(301);
+  } else {
+    
+    await db.collection("clientes").updateOne(
+        {"telefone": telefone},
+        { $inc: { "qdtPontos":  qtdPontos*-1}}
+     )
+     res.send(200);
+
+  }
+
+};
