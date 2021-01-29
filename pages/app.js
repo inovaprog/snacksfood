6512de3aded14a85d100cbcb91d236e00c723c8a
Wandinho Oliveira <inovaprog@gmail.com>
2021-01-29 08:29:25 -0300

    Create app.js

---
+++ pages/app.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+
+
+    return (
+        <div style={{
+            backgroundColor: "#6E2D2C",
+            padding: "20px",
+            height: "100%",
+            margin: 0,
+            borderRadius: 10
+
+        }}>
+            <center>
+                <a style={{
+                    textDecoration: 'none',
+                    color: "white"
+                }} href={`https://play.google.com/store/apps/details?id=br.com.inovaprog.snacksfood`}>BAIXAR O APLICATIVO SNACKSFOOD</a>
+            </center>
+        </div>
+    )
+}
+
+export default App
